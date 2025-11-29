058616ee034c867093402f86929e41b4f4ed1d63
thecorpsebutcher <thecorpsebutcher@gmail.com>
2025-11-29 13:36:33 +0100

    prueba pantallas

--- juego_flutter/test/widget_test.dart
+++ juego_flutter/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:juego_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyGameApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
